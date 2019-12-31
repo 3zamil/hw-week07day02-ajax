cf14675d6f9caaf948b555753b668a39a9f282b2
3zamil <3zamil@gmail.com>
2019-12-31 21:30:55 +0300

    first draft

---
+++ main.js
@@ -0,0 +1,40 @@
+
+let baseURL = 'https://api.imgflip.com/get_memes'
+
+function getInfo(info) {
+    if (info) {
+        axios.get(baseURL).then(function(result) {
+            res = result.data.data.memes
+            res.forEach(element => {
+                $("#content").append(`
+                <button onclick="$('#${element.id}').toggle()" style="font-size:24px">${element.name}</button><br>
+                <img id="${element.id}" src="${element.url}" style="width:${element.width}px;height:${element.height}px"><br>
+                `
+                )
+            });
+    
+        })
+       
+    } else {
+        axios.get(baseURL).then(function(result) {
+            res = result.data.data.memes
+            res.forEach(element => {
+                $("#content").append(`
+                <button onclick="$('#${element.id}').toggle()" style="font-size:24px">${element.name}</button><br>
+                <img id="${element.id}" src="${element.url}" style="width:${element.width}px;height:${element.height}px"><br>
+                `
+                )
+            });
+    
+        })
+    
+    }
+}
+    
+
+
+$(document).ready(function() {
+
+    getInfo('')
+
+})
