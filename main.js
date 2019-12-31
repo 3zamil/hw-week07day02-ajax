3ba7613bd13cc949bc2392abcda833a9aeffca0c
3zamil <3zamil@gmail.com>
2020-01-01 01:50:21 +0300

    Completed with bounce

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let baseURL = 'https://api.imgflip.com/get_memes'
 function getInfo(info) {
     if (info) {
         axios.get(baseURL).then(function(result) {
-            res = result.data.data.memes
+            res = result.data.data.memes.filter(word => word.name.includes(info));
+            $("#content").empty()
             res.forEach(element => {
                 $("#content").append(`
                 <button onclick="$('#${element.id}').toggle()" style="font-size:24px">${element.name}</button><br>
@@ -18,6 +19,7 @@ function getInfo(info) {
     } else {
         axios.get(baseURL).then(function(result) {
             res = result.data.data.memes
+            $("#content").empty()
             res.forEach(element => {
                 $("#content").append(`
                 <button onclick="$('#${element.id}').toggle()" style="font-size:24px">${element.name}</button><br>
